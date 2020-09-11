0ce7bd3bcf976758ea6e79e9cca14e07a5215da3
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-11 22:43:40 +0300

    my commit registering a blueprint

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+
+    NEWS_SOURCE_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL ='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 export NEWS_API_KEY ='1ac4b8b876a84da5a296bc2cbb9d276b'
+export SECRET_KEY = 'Flask WTF Secret Key'
