d1b007b682332b61d9c1d8ee38ed270f0ad40e0b
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-12 00:26:13 +0300

    my commit add a function to get source

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,6 @@
 import urllib.request,json
 from app.models import Source,Article
+import requests
 
 # Getting api key
 api_key = None
@@ -13,3 +14,32 @@ def configure_request(app):
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['NEWS_SOURCE_BASE_URL']
     base_url_article = app.config['ARTICLES_BASE_URL']
+
+def get_sources():
+    '''
+    Function that gets the json response from our url request
+    '''
+    get_source_url = base_url.format(api_key)
+    res = requests.get(get_source_url)
+    data = res.json().get('sources')
+    return process_sources(data)
+
+def process_sources(sources_list):
+    '''
+    Function  that processes the sources result and transforms them to a list of Objects
+    '''
+    sources_results = []
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        county = source_item.get('country')
+        
+        if url:
+            sources_object = Source(id, name, description, url, category, language, county)
+            sources_results.append(sources_object)
+        
+    return sources_results
