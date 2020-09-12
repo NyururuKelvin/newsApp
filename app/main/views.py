6878fb0577e6d719a19e29b3684cef9a8c008f7f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-12 21:59:39 +0300

    my commit add licence and views

---
+++ app/main/views.py
@@ -0,0 +1,19 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..request import get_sources, get_articles
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    sources = get_sources()
+    return render_template('index.html', sources = sources)
+    
+
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    articles = get_articles(source_id)
+    return render_template('article.html', articles=articles)
