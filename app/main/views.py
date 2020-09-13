49da2d75c4271964a2a2c790e30a17af799dbe29
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-13 14:14:41 +0300

    my commit add more templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,47 @@
 from . import main
 from flask import render_template,request,redirect,url_for
-from ..request import get_sources, get_articles
+from ..request import get_sources, get_article,search_for_article
 
 
 @main.route('/')
 def index():
-    '''
-    View root page function that returns the index page and its data
-    '''
+    articles=get_article()
+    sport=search_for_article('sports')
+    business=search_for_article('business')
+    sources=get_sources()
 
-    sources = get_sources()
-    return render_template('index.html', sources = sources)
+    #Make request to get article from server
+    search=request.args.get('search_name')
+
+    if search:
+        return redirect(url_for('main.search',article_name=search))
+    else:
+        return render_template('index.html',articles=articles,sports=sport,business=business,sources=sources)
     
 
-@main.route('/articles/<source_id>')
-def articles(source_id):
-    articles = get_articles(source_id)
-    return render_template('article.html', articles=articles)
+@main.route('/sports')
+def sources():
+    sport=search_for_article('sports')
+    search=request.args.get('search_name')
+    if search:
+        return redirect(url_for('main.search',article_name=search))
+    else:
+        return render_template('sports.html',sports=sport)
+
+@main.route('/business')
+def business():
+    business=search_for_article('business')
+    search=request.args.get('search_name')
+    if search:
+        return redirect(url_for('main.search',article_name=search))
+    else:
+        return render_template('business.html',business=business)
+
+@main.route('/search/<article_name>')
+def search(article_name):
+    artcle_name_list=article_name.split(" ")
+    artcle_name_format="+".join(artcle_name_list)
+    searched_article=search_for_article(artcle_name_format)
+    title=f'{article_name}'
+
+    return render_template('search.html',title=title,article=searched_article)
